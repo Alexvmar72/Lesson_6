4b63a7289483225ce377fdabb1ff322a1d929d9d
Alex Mark <alexvmar@yandex.ru>
2023-03-03 09:41:26 +0300

    change files homework6

--- task_6_2/Program.cs
+++ task_6_2/Program.cs
@@ -4,5 +4,25 @@
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)           */
 
+Console.Clear();
 
+double EnterCoord(string message)
+{
+    Console.Write(message);
+    double result = double.Parse(Console.ReadLine()!);
+    return result;
+}
 
+Console.WriteLine("Введите два значения первого уравнения");
+double b1 = EnterCoord("b1: ");
+double k1 = EnterCoord("k1: ");
+
+Console.WriteLine("Введите два значения второго уравнения ");
+double b2 = EnterCoord("b2: ");
+double k2 = EnterCoord("k2: ");
+
+double x = (b2 - b1) / (k1 - k2);
+
+double y = k1 * x + b1;
+
+Console.WriteLine($"Координата пересечения прямых, заданных уравнениями: Х={x}, Y={y}");
